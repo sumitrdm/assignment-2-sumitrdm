820700d8f5f1a3937e261019f5149527fad2fd6f
Sumit kumar <59776924+sumitrdm@users.noreply.github.com>
2020-03-28 18:47:52 +0530

    getter setter of the datamembers of the class

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -11,9 +11,35 @@ public class Node {
     int roll_no;
     Node next;
 
+
     public Node(String name, int roll_no) {
         this.name = name;
         this.roll_no = roll_no;
         this.next = null;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRoll_no() {
+        return roll_no;
+    }
+
+    public void setRoll_no(int roll_no) {
+        this.roll_no = roll_no;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
     }
 }
