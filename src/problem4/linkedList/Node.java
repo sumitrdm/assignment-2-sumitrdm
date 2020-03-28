d82cf4ba2706d1e71cb085706e68587704d1221e
Sumit kumar <59776924+sumitrdm@users.noreply.github.com>
2020-03-28 20:19:02 +0530

    getter setter of the data field,.

--- src/problem4/linkedList/Node.java
+++ src/problem4/linkedList/Node.java
@@ -8,6 +8,14 @@ public class Node {
         this.next = null;
     }
 
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
     public void setNext(Node node) {
     }
 
