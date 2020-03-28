1d31976441161fd57d4c38c5e1bbdffcc10b8b9d
Sumit kumar <59776924+sumitrdm@users.noreply.github.com>
2020-03-28 18:12:34 +0530

    method created insert.

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -17,4 +17,26 @@ public class MyBinarySearchTree {
         this.root = root;
     }
 
+    public void insert(int data) {
+        TreeNode newnode = new TreeNode(data);
+        if (root == null) {
+            root = newnode;
+        } else {
+            TreeNode temp = root;
+            TreeNode parent = null;
+            while (temp != null) {
+                parent = temp;
+                if (newnode.getData() >= temp.getData())
+                    temp = temp.getRight();
+                else
+                    temp = temp.getLeft();
+            }
+            if (newnode.getData() <= parent.getData())
+                parent.setLeft(newnode);
+            else
+                parent.setRight(newnode);
+        }
+    }
+
+
 }
