28f234890c647d843b634df08f904b468703fdb4
Sumit kumar <59776924+sumitrdm@users.noreply.github.com>
2020-03-28 20:29:34 +0530

    create a method name as search which searches the data..

--- src/problem4/linkedList/MyLinkedList.java
+++ src/problem4/linkedList/MyLinkedList.java
@@ -49,7 +49,16 @@ public class MyLinkedList {
         this.last = last;
     }
 
-
+    public Node Search(int data) {
+        var current = first;
+        while (current != null) {
+            if (current.getData() == data) {
+                return current;
+            }
+            current = current.getNext();
+        }
+        return null;
+    }
 }
 
 
