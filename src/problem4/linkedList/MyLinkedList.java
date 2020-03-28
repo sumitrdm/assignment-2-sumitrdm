cc2e52969aea45eec30c6cd34dc339f2d9192a7a
Sumit kumar <59776924+sumitrdm@users.noreply.github.com>
2020-03-28 19:54:13 +0530

    create a method name addLast.

--- src/problem4/linkedList/MyLinkedList.java
+++ src/problem4/linkedList/MyLinkedList.java
@@ -4,5 +4,17 @@ public class MyLinkedList {
     private Node first;
     private Node last;
 
+    public void addLast(int data) {
+        Node node = new Node(data);
+        if (first == last) {
+            first = last = node;
+            return;
+        }
+        last.setNext(node);
+        last = node;
+
+
+    }
+
 
 }
--- src/problem4/linkedList/Node.java
+++ src/problem4/linkedList/Node.java
@@ -1,4 +1,9 @@
 package problem4.linkedList;
 
 public class Node {
+    public Node(int data) {
+    }
+
+    public void setNext(Node node) {
+    }
 }
