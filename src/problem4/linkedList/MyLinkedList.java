d9e5e208afe8e6805d37a7f5ef61eb4b783af428
Sumit kumar <59776924+sumitrdm@users.noreply.github.com>
2020-03-28 19:48:10 +0530

    create a MyLinkedList class and write the datamembers as private.

---
+++ src/problem4/linkedList/MyLinkedList.java
@@ -0,0 +1,8 @@
+package problem4.linkedList;
+
+public class MyLinkedList {
+    private Node first;
+    private Node last;
+
+
+}
--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -4,8 +4,32 @@
  *  Date: 23-Mar-20
  *  Time: 7:11 PM
  */
-package problem4.myqueue;
+/*package problem4.myqueue;
 // to create queue to store pre - order successor
 
+import problem4.linkedList.MyLinkedList;
+
 public class MyQueue {
+
+    MyLinkedList linkedList = new MyLinkedList();
+
+    public int peek(int data) {
+        if (linkedList.search(data) == null) {
+            return -1;
+        }
+        return linkedList.search(data).getData();
+    }
+
+    public void enqueue(int data) {
+        linkedList.addLast(data);
+    }
+
+    public void dequeue() {
+        linkedList.deleteFirst();
+    }
+
+    public void show() {
+        linkedList.display();
+    }
 }
+*/
