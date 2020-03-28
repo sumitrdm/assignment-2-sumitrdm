1e3f91c867c20c7d6b686f3167214fb0e077a942
Sumit kumar <59776924+sumitrdm@users.noreply.github.com>
2020-03-28 18:54:26 +0530

    main class completed by importing MypriorityQueue class.

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,18 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue p = new MyPriorityQueue();
+        p.push("a", 1);
+        p.push("b", 2);
+        p.push("c", 3);
+
+        p.display();
+    }
 }
