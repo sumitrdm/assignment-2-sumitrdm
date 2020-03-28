0b8312364e870d9a920ca11d62be81df57304ac5
Sumit kumar <59776924+sumitrdm@users.noreply.github.com>
2020-03-28 23:01:27 +0530

    finish of the class MyBinarySearchTree

--- src/problem4/binaryseachtree/MyBinarySearchTree.java
+++ src/problem4/binaryseachtree/MyBinarySearchTree.java
@@ -10,4 +10,57 @@ public class MyBinarySearchTree {
     public MyBinarySearchTree(MyQueue queue) {
         this.myQueue = queue;
     }
+
+    public void insert(int data) {
+        TreeNode node = new TreeNode(data);
+        if (root == null) {
+            root = node;
+        } else {
+            TreeNode current = root;
+            TreeNode parent = null;
+            while (current != null) {
+                parent = current;
+                if (node.getData() < current.getData()) {
+                    current = current.getLeft();
+                } else {
+                    current = current.getRight();
+                }
+            }
+            if (node.getData() < parent.getData()) {
+                parent.setLeft(node);
+            } else {
+                parent.setRight(node);
+            }
+        }
+    }
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public void setRoot(TreeNode root) {
+        this.root = root;
+    }
+
+    public void preOrder(TreeNode temp) {
+        if (temp == null) {
+            return;
+        } else {
+//            System.out.println(temp.getData());
+            myQueue.enqueue(temp.getData());
+            preOrder(temp.getLeft());
+            preOrder(temp.getRight());
+        }
+    }
+
+    public void inOrder(TreeNode temp) {
+        if (temp == null) {
+            return;
+        } else {
+
+            inOrder(temp.getLeft());
+            System.out.println(temp.getData());
+            inOrder(temp.getRight());
+        }
+    }
 }
