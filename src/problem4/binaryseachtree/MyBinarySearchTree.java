c016ff6e95ed3f99b4cc15d55b2e8b5144c4d901
Sumit kumar <59776924+sumitrdm@users.noreply.github.com>
2020-03-28 20:44:09 +0530

    create parametrised constructor and the data members of the class.

--- src/problem4/binaryseachtree/MyBinarySearchTree.java
+++ src/problem4/binaryseachtree/MyBinarySearchTree.java
@@ -6,4 +6,8 @@ import problem4.myqueue.MyQueue;
 public class MyBinarySearchTree {
     private MyQueue myQueue;
     private TreeNode root;
+
+    public MyBinarySearchTree(MyQueue queue) {
+        this.myQueue = queue;
+    }
 }
