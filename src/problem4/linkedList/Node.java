2cf3bb114b0767605fe5d3fddddc20892e31e563
Sumit kumar <59776924+sumitrdm@users.noreply.github.com>
2020-03-28 20:17:34 +0530

    create paremetrised constructor.

--- src/problem4/linkedList/Node.java
+++ src/problem4/linkedList/Node.java
@@ -4,6 +4,8 @@ public class Node {
     private int data;
     private Node next;
     public Node(int data) {
+        this.data = data;
+        this.next = null;
     }
 
     public void setNext(Node node) {
