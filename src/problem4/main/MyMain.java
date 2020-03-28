f7233dc85e5f28841f7da529eed1605eee9dd1a4
Sumit kumar <59776924+sumitrdm@users.noreply.github.com>
2020-03-28 23:25:11 +0530

    import problem4 classes in the main.

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -5,7 +5,17 @@
  *  Time: 7:08 PM
  */
 package problem4.main;
+
+import problem4.binaryseachtree.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        MyQueue myQueue = new MyQueue();
+        MyBinarySearchTree myBinarySearchTree = new MyBinarySearchTree(myQueue);
+        
+    }
+
 }
